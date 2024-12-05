1b108f396e1f068706e836f6b4b86847d16d7398
Louis Charvet <locharve@3F5.42angouleme.fr>
2024-12-05 11:49:46 +0100

    correction: width and height -= 2

--- src/t_cub_set_map.c
+++ src/t_cub_set_map.c
@@ -6,7 +6,7 @@
 /*   By: locharve <locharve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 12:17:09 by locharve          #+#    #+#             */
-/*   Updated: 2024/11/02 11:26:32 by locharve         ###   ########.fr       */
+/*   Updated: 2024/12/05 11:48:57 by locharve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,6 +100,10 @@ t_err	t_cub_set_map(t_cub *cub, char **raw)
 			cub->err = smooth_map(cub, &cub->map);
 		if (!cub->err)
 			cub->err = check_map(cub, &cub->map);
+		if (!cub->err) {
+			cub->height -= 2;
+			cub->width -= 2;
+		}
 		strtab_free(avtab);
 	}
 	return (cub->err);
